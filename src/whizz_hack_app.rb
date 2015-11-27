9e655ad024aef143fbee348bcb948a30d77480b4
Carlos <toxz@foxmail.com>
2015-11-27 20:15:03 +0800

    add upload api

--- src/whizz_hack_app.rb
+++ src/whizz_hack_app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'json'
+require 'haml'
 
 
 get '/' do
@@ -7,10 +8,34 @@ get '/' do
 end
 
 
-get '/user/add/:name' do
+get '/user/add' do
+  name = params['hostname']
+  id = params['id']
+  ip = params['ip']
+  model = params['model']
+
   return_message = {}
-  if params.has_key?('name')
+  if params.has_key?('hostname') and params.has_key?('id')
     return_message[:status] = 'success'
+    return_message[:name] = name
+    return_message[:ip] = request.ip
   end
   return_message.to_json
+end
+
+get '/user/:id/upload' do
+  haml :upload
+end
+
+post '/user/:id/upload' do
+
+  dir = File.expand_path(params[:id],'uploads')
+  unless File.directory?(dir)
+    FileUtils.mkdir_p(dir)
+  end
+  
+  File.open('uploads/'+params[:id]+'/'+params['file'][:filename], 'w') do |f|
+    f.write(params['file'][:tempfile].read)
+  end
+  {:status => 'success'}.to_json
 end
